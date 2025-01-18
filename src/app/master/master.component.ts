c18931cbcba6340ff5fb69a3e24fce4f256889d5
SriChatala <sri.chatala@finning.com>
2025-01-17 23:00:11 -0500

    added roles and destination c in maste c

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { DesignationComponent } from '../components/designation/designation.component';
+import { RolesComponent } from '../components/roles/roles.component';
 
 @Component({
   selector: 'app-master',
-  imports: [],
+  imports: [DesignationComponent,RolesComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss'
 })
 export class MasterComponent {
-
+currentComponent:string ="";
 }
