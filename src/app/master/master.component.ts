d856078066dd2d3c57d05b863ee947ec1832631b
SriChatala <sri.chatala@finning.com>
2025-01-19 23:15:02 -0500

    using directives

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { DesignationComponent } from '../components/designation/designation.component';
 import { RolesComponent } from '../components/roles/roles.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-master',
-  imports: [DesignationComponent,RolesComponent],
+  imports: [DesignationComponent,RolesComponent,CommonModule],
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss'
 })
 export class MasterComponent {
-currentComponent:string ="";
+currentComponent:string ="Roles";
+changeTab(tabName:string){
+  this.currentComponent =tabName;
+}
 }
