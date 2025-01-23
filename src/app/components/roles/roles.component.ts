7be75c3c91c16043b2a802be20127f931ea846c0
SriChatala <sri.chatala@finning.com>
2025-01-22 23:21:25 -0500

    we call the api

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -7,18 +9,67 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss'
 })
-export class RolesComponent {
-  firstName :string ="angular tutiorals";
-  angularVersion ="Verson 18";
-  version:number = 18;
-  iaActive : boolean = false;
-  currentDate : Date = new Date();
-  inputType:string = "checkbox";
-  selectedState :string ="";
-  showWelcomeAlert(){
-    alert("welcome angular tutiorals")
+export class RolesComponent implements OnInit {
+
+
+  roleList:IRole[] = [];
+  http = inject(HttpClient);
+
+
+  ngOnInit(): void {
+    this.getAllRoles()
   }
-  showMessage(message: string){
-    alert(message)
+
+  getAllRoles(){
+    this.http.get('https://fake-json-api.mock.beeceptor.com/users').subscribe((r:any)=>{
+      this.roleList = r;
+    })
   }
+  
 }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // firstName :string ="angular tutiorals";
+  //angularVersion ="Verson 18";
+  //version:number = 18;
+  //iaActive : boolean = false;
+ // currentDate : Date = new Date();
+  //inputType:string = "checkbox";
+  //selectedState :string ="";
+  //showWelcomeAlert(){
+    //alert("welcome angular tutiorals")
+  //}
+  //showMessage(message: string){
+    //alert(message)
+  //}
+//}
